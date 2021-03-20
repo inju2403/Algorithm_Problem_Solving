d040dbe8fd2a59ff35be1cc482f8e3f08d6b3a22
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-20 11:43:45 +0900

    add 1969.cpp

---
+++ BOJ/1969.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include <vector>
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define FORS(i,s,n) for(int i=(s);i<(n);++i)
+using namespace std;
+
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+}
