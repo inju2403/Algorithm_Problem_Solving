0902b20ec4d338e23b5cda25a19dba57a7b600f4
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-03-20 13:57:00 +0900

    update BOJ solve

--- BOJ/1292.cpp
+++ BOJ/1292.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 #include<algorithm>
-#include <vector>
 #define FOR(i,n) for(int i=0;i<(n);++i)
 #define FORS(i,s,n) for(int i=(s);i<(n);++i)
 using namespace std;
 
-
+int x,y,num=1,i=1,sum,a[1050];
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
+    cin>>x>>y;
+    while(i<y+1) {
+        FOR(j,num) a[i++]=num;
+        num++;
+    }
+    FORS(j,x,y+1) sum+=a[j];
+    cout<<sum;
 }
--- BOJ/1969.cpp
+++ BOJ/1969.cpp
@@ -1,12 +1,34 @@
 #include<iostream>
 #include<algorithm>
-#include <vector>
+#include<vector>
+#include<string>
+#include<unordered_map>
 #define FOR(i,n) for(int i=0;i<(n);++i)
 #define FORS(i,s,n) for(int i=(s);i<(n);++i)
 using namespace std;
 
-
+int n,l,mn;
+vector<string> v;
+string s;
+unordered_map<char,int> um;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
+    cin>>n>>l;
+    v.resize(n);
+    FOR(i,n) cin>>v[i];
+    FOR(i,l) {
+        int mx=0;
+        char ch='Z';
+        FOR(j,n) um[v[j][i]]++;
+        FOR(j,n) if(um[v[j][i]] >= mx) {
+            if(um[v[j][i]]>mx) ch = v[j][i];
+            else if(um[v[j][i]]==mx && ch>v[j][i]) ch = v[j][i];
+            mx = um[v[j][i]];
+        }
+        mn+=n-mx;
+        s+=ch;
+        um.clear();
+    }
+    cout<<s<<'\n'<<mn;
 }
