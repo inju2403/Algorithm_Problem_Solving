c30f64d03cb9bfb92b8ba1702322e39568981c4a
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-05-07 00:33:24 +0900

    add 9659.cpp

---
+++ BOJ/9659.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<unordered_map>
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define FORS(i,s,n) for(int i=(s);i<(n);++i)
+#define INF 987654321
+#define MAX 1010
+using namespace std;
+typedef pair<int,int> pii;
+typedef long long ll;
+
+ll n;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    cin>>n;
+    cout<<(n%2? "SK" : "CY");
+}
