3a42da0ef76f30d46209b4d42dd25a832bfe2555
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-05-19 00:03:19 +0900

    add 17352_1.cpp

---
+++ BOJ/17352_1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<unordered_map>
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define FORS(i,s,n) for(int i=(s);i<(n);++i)
+#define INF 987654321
+#define MAX 300010
+using namespace std;
+typedef pair<int,int> pii;
+typedef long long ll;
+
+int n,a,b,parent[MAX];
+
+int getParent(int x) {
+    return x == parent[x]? x : parent[x]=getParent(parent[x]);
+}
+
+void merge(int a,int b) {
+    a = getParent(a), b = getParent(b);
+    a < b? parent[b]=a : parent[a]=b;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    cin>>n;
+    FORS(i,1,n+1) parent[i]=i;
+    FOR(i,n-2) {
+        cin>>a>>b;
+        if(getParent(a)!=getParent(b)) {
+            merge(a,b);
+        }
+    }
+    FORS(i,1,n+1) if(i==getParent(i)) cout<<i<<" ";
+}
