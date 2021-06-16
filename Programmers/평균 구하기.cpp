892c44a3f695bcae6c95a40ae5f14124daf14938
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-17 00:01:33 +0900

    add 평균 구하기.cpp

---
+++ Programmers/평균 구하기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for(auto x: arr) answer+=x;
+    return answer/arr.size();
+}
