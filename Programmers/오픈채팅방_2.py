21278bcdd9846223ea39dd573d57cf8be09a7d26
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-07-20 00:01:30 +0900

    add 오픈채팅방_2.py

---
+++ Programmers/오픈채팅방_2.py
@@ -0,0 +1,22 @@
+def solution(record):
+    res = []
+    id = []
+    mp = {}
+    exp = []
+    
+    for x in record:
+        a = x.split(" ");
+        if a[0]=="Enter":
+            mp[a[1]]=a[2]
+            id.append(a[1])
+            exp.append("님이 들어왔습니다.")
+        elif a[0]=="Leave":
+            id.append(a[1])
+            exp.append("님이 나갔습니다.")
+        else:
+            mp[a[1]]=a[2]
+            
+    for i in range(0,len(exp)):
+        res.append(mp[id[i]]+exp[i])
+    
+    return res
