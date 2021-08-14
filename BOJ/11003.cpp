be9e154d5c0a48db469e5cd516741ca7253179a0
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-08-14 21:58:17 +0900

    add 11003.cpp

---
+++ BOJ/11003.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<unordered_map>
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define FORS(i,s,n) for(int i=(s);i<(n);++i)
+#define INF 987654321
+#define LINF 9123456789123456789
+#define MAX 1005
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int n,l,x,i;
+priority_queue<pii> pq;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    cin>>n>>l;
+    while(n--) {
+        cin>>x;
+        pq.push({-x,i});
+        while(pq.top().second < i-l+1) pq.pop();
+        cout<<-pq.top().first<<" ";
+        ++i;
+    }
+}
