41e2465eddc9c1c28dbf202ad79da9c78149173c
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-08-19 00:05:10 +0900

    add 1285.cpp

--- BOJ/1285.cpp
+++ BOJ/1285.cpp
@@ -1,27 +1,46 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
+#include<queue>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<unordered_map>
 #define FOR(i,n) for(int i=0;i<(n);++i)
 #define FORS(i,s,n) for(int i=(s);i<(n);++i)
+#define INF 987654321
+#define LINF 2123456789123456789
+#define MAX 21
 using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
 
-int n,m;
-vector<int> v;
-
-void dfs(int x,vector<int> &v) {
-    if(v.size()==m) {
-        FOR(i,m) cout<<v[i]<<" ";
-        cout<<'\n';
-        return;
-    }
-    FORS(next,x,n+1) {
-        v.push_back(next);
-        dfs(next+1,v);
-        v.pop_back();
-    }
-}
+int n,res=INF;
+vector<string> mp,ori;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
-    cin>>n>>m;
-    dfs(1,v);
+    cin>>n;
+    mp.resize(n); ori.resize(n);
+    FOR(i,n) cin>>mp[i];
+    FOR(i,n) ori[i]=mp[i];
+    FOR(i,1<<n) {
+        FOR(y,n) {
+            if((i&(1<<y)) == (1<<y)) {
+                FOR(x,n) mp[y][x]=='T'? mp[y][x]='H' : mp[y][x]='T';
+            }
+        }
+        FOR(x,n) {
+            int cnt = 0;
+            FOR(y,n) if(mp[y][x]=='T') ++cnt;
+            if(cnt>n-cnt) FOR(y,n) mp[y][x]=='T'? mp[y][x]='H' : mp[y][x]='T';
+        }
+        
+        int cur = 0;
+        FOR(y,n) FOR(x,n) if(mp[y][x]=='T') ++cur;
+        res = min(res,cur);
+        FOR(i,n) mp[i]=ori[i];
+    }
+    cout<<res;
 }
