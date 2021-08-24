c96cd63a7b1a4a29aa9946b6cdba1b3ebd199e27
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-08-25 00:02:33 +0900

    add 큰 수 만들기.swift

---
+++ Programmers/Swift/큰 수 만들기.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+func solution(_ number:String, _ K:Int) -> String {
+    var k = K
+    var cnt = 0
+    
+    var numbers = Array(number).map({Int(String($0))!})
+    var res: [Int] = []
+    
+    for i in 0..<numbers.count {
+        if  res.isEmpty == true {
+            res.append(numbers[i])
+        } else {
+            while res.isEmpty == false
+            && res.last! < numbers[i]
+            && k>0 {
+                res.removeLast()
+                k-=1
+            }
+            
+            if k==0 {
+                for j in i..<numbers.count {
+                    res.append(numbers[j])
+                }
+                break
+            }
+            res.append(numbers[i])
+        }
+    }
+    while k>0 {
+        res.removeLast()
+        k-=1
+    }
+    return res.map({String($0)}).joined()
+}
