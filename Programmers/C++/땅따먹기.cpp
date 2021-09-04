04645a1589671fc4855e688022a0c67552c71069
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-09-04 12:12:36 +0900

    add 땅따먹기.cpp

---
+++ Programmers/C++/땅따먹기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define FORS(i,s,n) for(int i=(s);i<(n);++i)
+using namespace std;
+
+int mx;
+
+int solution(vector<vector<int> > land) {
+    FORS(y,1,land.size()) {
+        FOR(x,land[0].size()) {
+            int mx = 0;
+            FOR(px,land[0].size()) {
+                if(x==px) continue;
+                mx = max(mx,land[y-1][px]);
+            }
+            land[y][x]+=mx;
+        }
+    }
+
+    FOR(x,land[0].size()) mx=max(mx,land[land.size()-1][x]);
+    return mx;
+}
