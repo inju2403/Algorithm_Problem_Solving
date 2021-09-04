a257f841a6698d6d1d9429b497f4a01d6523ce15
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-09-04 19:39:37 +0900

    add 메뉴 리뉴얼_2.cpp

---
+++ Programmers/C++/메뉴 리뉴얼_2.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define FORS(i,s,n) for(int i=(s);i<(n);++i)
+
+using namespace std;
+
+vector<string> res;
+unordered_map<string, int> um[11];
+
+void dfs(int idx, int cnt, string& s, string& cur) {
+    if(cur.size()==cnt) {
+        ++um[cnt][cur];
+        return;
+    }
+    FORS(i,idx,s.size()) {
+        cur+=s[i];
+        dfs(i+1,cnt,s,cur);
+        cur.pop_back();
+    }
+}
+
+vector<string> solution(vector<string> orders, vector<int> course) {
+    
+    for(string s: orders) {
+        sort(s.begin(),s.end());
+        FORS(i,2,s.size()+1) {
+            string cur = "";
+            dfs(0,i,s,cur);
+        }
+    }
+    
+    for(int n: course) {
+        int mx = 0;
+        for(auto x: um[n]) mx=max(mx,x.second);
+        if(mx<=1) continue;
+        for(auto x: um[n]) if(mx==x.second) res.push_back(x.first);
+    }
+    
+    sort(res.begin(), res.end());
+    
+    return res;
+}
