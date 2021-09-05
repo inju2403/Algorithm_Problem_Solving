bd2511856be55e5b0c5175f6380b296f4df6979f
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-09-06 00:19:33 +0900

    add 11170.cpp

---
+++ BOJ/11170.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<unordered_map>
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define FORS(i,s,n) for(int i=(s);i<(n);++i)
+#define INF 987654321
+#define LINF 9123456789123456789
+#define MAX 30
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+int tc,n,m,res;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    cin>>tc;
+    while(tc--) {
+        res=0;
+        cin>>n>>m;
+        FORS(i,n,m+1) {
+            string s = to_string(i);
+            for(auto c: s) if(c=='0') ++res;
+        }
+        cout<<res<<'\n';
+    }
+}
