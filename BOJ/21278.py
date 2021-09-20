adebdf38296141a82897567fd66d2c5e058cdc63
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-09-20 11:40:36 +0900

    Update 21278.py

--- BOJ/21278.py
+++ BOJ/21278.py
@@ -33,7 +33,7 @@ def floyd():
 floyd()
 
 for i in range(1,n+1):
-    for j in range(1,n+1):
+    for j in range(i+1,n+1):
         if i==j: continue
         sum = 0
         for k in range(1,n+1):
